91ff3316afe67ab34a97a909baa4e478ceab766d
Peter Dietz <dietz.72@osu.edu>
2011-07-29 17:59:12 -0400

    Exporting item growth statistics to CSV.

--- dspace-api/src/main/java/org/dspace/app/statistics/GrowthStatistics.java
+++ dspace-api/src/main/java/org/dspace/app/statistics/GrowthStatistics.java
@@ -6,14 +6,17 @@ import au.com.bytecode.opencsv.CSVWriter;
 import org.apache.log4j.Logger;
 import org.dspace.core.Context;
 import org.dspace.storage.rdbms.DatabaseManager;
+import org.dspace.storage.rdbms.TableRow;
 import org.dspace.storage.rdbms.TableRowIterator;
 
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.transform.Result;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -35,17 +38,43 @@ public class GrowthStatistics extends HttpServlet
         CSVWriter writer = new CSVWriter(response.getWriter());
 
         // feed in your array (or convert your data to an array)
-        String[] entries = "first#second#third".split("#");
-        writer.writeNext(entries);
+        //String[] entries = "first#second#third".split("#");
+        //writer.writeNext(entries);
 
+        String[] firstRow = new String[3];
+        firstRow[0] = "YYYY-MM";
+        firstRow[1] = "countitems";
+        firstRow[2] = "totalitems";
+        writer.writeNext(firstRow);
 
+        TableRowIterator tri = null;
+        try {
+            tri = itemGrowth();
+
+            Integer totalItems = 0;
+            while(tri.hasNext()) {
+                TableRow row = tri.next();
+                //log.debug(row.toString());
+                String date = row.getStringColumn("yearmo");
+                Long numItems = row.getLongColumn("countitem");
+                totalItems += numItems.intValue();
+                String[] rowString = new String[3];
+                rowString[0] = date;
+                rowString[1] = numItems.toString();
+                rowString[2] = totalItems.toString();
+
+                writer.writeNext(rowString);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
 
         writer.close();
 
 
     }
 
-    protected void itemGrowth() throws SQLException {
+    protected TableRowIterator itemGrowth() throws SQLException {
         Context context = new Context();
 
         String query = "SELECT to_char(date_trunc('month', t1.ts), 'YYYY-MM') AS yearmo, count(*) as countitem " +
@@ -53,6 +82,8 @@ public class GrowthStatistics extends HttpServlet
                 "WHERE metadata_field_id = 12 AND metadatavalue.item_id = item.item_id AND item.in_archive=true	) t1 " +
                 "GROUP BY date_trunc('month', t1.ts) order by yearmo asc;";
         TableRowIterator tri = DatabaseManager.query(context, query);
-        //DatabaseManager.query().toList().toArray()
+
+        return tri;
+
     }
 }
