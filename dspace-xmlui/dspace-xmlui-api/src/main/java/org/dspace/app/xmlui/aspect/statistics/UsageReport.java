1ac411c86d7ab4c74b53125234abdbf2a3bf1abf
Peter Dietz <dietz.72@osu.edu>
2011-09-23 17:45:49 -0400

    Sorting facets according to count in all cases.

---
+++ dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/statistics/UsageReport.java
@@ -0,0 +1,11 @@
+package org.dspace.app.xmlui.aspect.statistics;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: peterdietz
+ * Date: 9/23/11
+ * Time: 12:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UsageReport {
+}
