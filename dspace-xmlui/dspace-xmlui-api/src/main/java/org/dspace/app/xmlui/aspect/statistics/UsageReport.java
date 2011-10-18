c4ffa1a2c1eab8662d1e616c56d80a645a1453e3
Peter Dietz <dietz.72@osu.edu>
2011-10-18 18:21:05 -0400

    Adds item and collection to each bitstream reported in usage-report servlet.

--- dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/statistics/UsageReport.java
+++ dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/statistics/UsageReport.java
@@ -64,13 +64,17 @@ public class UsageReport extends HttpServlet {
     public void addTypeDownloadsForOwningContainer(DSpaceObject parentDSO, CSVWriter writer, Integer reportType) throws SQLException {
         // http://localhost:8080/solr/statistics/select?q=type:0+AND+owningComm:167&facet=true&facet.field=id&rows=0&facet.limit=-1&facet.mincount=1&facet.sort=count
 
-        String[] headerRow = new String[4];
+        String[] headerRow = new String[8];
         switch(reportType) {
             case Constants.BITSTREAM:
                 headerRow[0] = "bitstreamID";
                 headerRow[1] = "bitstreamName";
                 headerRow[2] = "bundleName";
                 headerRow[3] = "Downloads";
+                headerRow[4] = "ItemHandle";
+                headerRow[5] = "ItemName";
+                headerRow[6] = "CollectionHandle";
+                headerRow[7] = "CollectionName";
                 break;
             case Constants.ITEM:
                 headerRow[0] = "itemID";
@@ -105,7 +109,7 @@ public class UsageReport extends HttpServlet {
 
 
         for(ObjectCount objectCount : objectCounts) {
-            String[] row = new String[4];
+            String[] row = new String[8];
 
             switch (reportType) {
                 case Constants.BITSTREAM:
@@ -115,6 +119,28 @@ public class UsageReport extends HttpServlet {
                     row[1] = bitstream.getName();
                     row[2] = (bitstream.getBundles().length>0) ? bitstream.getBundles()[0].getName() : "unknown";
                     row[3] = String.valueOf(objectCount.getCount());
+
+                    // Add additional information about the bitstream: owning Item, Collection
+                    DSpaceObject bitstreamParentDSO = bitstream.getParentObject();
+                    if(bitstreamParentDSO instanceof Item) {
+                        Item parentItem = (Item) bitstreamParentDSO;
+                        row[4] = parentItem.getHandle();
+                        row[5] = parentItem.getName();
+                        Collection[] owningCollections = parentItem.getCollections();
+                        row[6] = "";
+                        row[7] = "";
+                        for(int i =0; i < owningCollections.length; i++) {
+                            if(i>0) {
+                                // Add a delimiter between multiple entries
+                                row[6] += "||";
+                                row[7] += "||";
+                            }
+                            row[6] += owningCollections[i].getHandle();
+                            row[7] += owningCollections[i].getName();
+                        }
+                    }
+
+
                     break;
                 case Constants.ITEM:
                     Integer itemID = Integer.parseInt(objectCount.getValue());
@@ -123,6 +149,7 @@ public class UsageReport extends HttpServlet {
                     row[1] = item.getName();
                     row[2] = item.getHandle();
                     row[3] = String.valueOf(objectCount.getCount());
+                    //@TODO Add more context infor for item
             }
 
             writer.writeNext(row);
