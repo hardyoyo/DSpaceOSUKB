264348ce290a8c8aba2cbb61c8ec0b20cf5de53a
Peter Dietz <dietz.72@osu.edu>
2012-01-13 17:00:00 -0500

    Made GrowthStatistics dashboard report work.

--- dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/dashboard/GrowthStatistics.java
+++ dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/dashboard/GrowthStatistics.java
@@ -3,18 +3,31 @@ package org.dspace.app.xmlui.aspect.dashboard;
 
 
 import au.com.bytecode.opencsv.CSVWriter;
+import org.apache.avalon.excalibur.pool.Recyclable;
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.ProcessingException;
+import org.apache.cocoon.environment.ObjectModelHelper;
+import org.apache.cocoon.environment.Request;
+import org.apache.cocoon.environment.Response;
+import org.apache.cocoon.environment.SourceResolver;
+import org.apache.cocoon.reading.AbstractReader;
 import org.apache.log4j.Logger;
 import org.dspace.app.xmlui.cocoon.AbstractDSpaceTransformer;
+import org.dspace.app.xmlui.utils.ContextUtil;
 import org.dspace.core.Context;
 import org.dspace.storage.rdbms.DatabaseManager;
 import org.dspace.storage.rdbms.TableRow;
 import org.dspace.storage.rdbms.TableRowIterator;
+import org.xml.sax.SAXException;
 
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.sql.SQLException;
+import java.util.Map;
 
 /**
  * Exporting Tchera's stats in CSV format.
@@ -23,60 +36,65 @@ import java.sql.SQLException;
  * Time: 11:56 AM
  * To change this template use File | Settings | File Templates.
  */
-public class GrowthStatistics extends AbstractDSpaceTransformer
+public class GrowthStatistics extends AbstractReader implements Recyclable
 {
     protected static final Logger log = Logger.getLogger(GrowthStatistics.class);
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/csv; encoding='UTF-8'");
-        response.setStatus(HttpServletResponse.SC_OK);
-        CSVWriter writer = new CSVWriter(response.getWriter());
+    /** The Cocoon response */
+    protected Response response;
 
-        TableRowIterator tri;
+    /** The Cocoon request */
+    protected Request request;
 
-        //Allow for the content type to be passed in. 0 = BITSTREAM, 1 = ITEM, ...
-        String typeParam = request.getParameter("type");
-        String typeString = null;
+    private CSVWriter writer;
+    
+    private String typeString;
 
-        Integer type = null;
-        if(typeParam == null) {
-            type = 1;
-        } else {
-            type = Integer.parseInt(typeParam);
-        }
-        log.info("Got parameter type:"+type);
-
-        switch(type) {
-            case 0:
-                typeString = "bitstream";
-                tri = bitstreamGrowth();
-                break;
-            case 1:
-            default:
-                typeString = "item";
-                tri = itemGrowth();
-        }
-
-        response.setHeader("Content-Disposition", "attachment; filename=growth-"+typeString+".csv");
-
-
-        // feed in your array (or convert your data to an array)
-        //String[] entries = "first#second#third".split("#");
-        //writer.writeNext(entries);
-
-        String[] firstRow = new String[3];
-        firstRow[0] = "YYYY-MM";
-        firstRow[1] = "count";
-        firstRow[2] = "total"+typeString;
-        writer.writeNext(firstRow);
-
-
+    public void setup(SourceResolver resolver, Map objectModel, String src, Parameters par)
+            throws ProcessingException, SAXException, IOException
+    {
+        super.setup(resolver, objectModel, src, par);
         try {
+            this.request = ObjectModelHelper.getRequest(objectModel);
+            this.response = ObjectModelHelper.getResponse(objectModel);
+            Context context = ContextUtil.obtainContext(objectModel);
+
+            //Allow for the content type to be passed in. 0 = BITSTREAM, 1 = ITEM, ...
+            String typeParam = request.getParameter("type");
+            Integer type;
+            if(typeParam == null) {
+                type = 1;
+            } else {
+                type = Integer.parseInt(typeParam);
+            }
+            log.info("Got parameter type:"+type);
+
+            TableRowIterator tri;
+            switch(type) {
+                case 0:
+                    typeString = "bitstream";
+                    tri = bitstreamGrowth(context);
+                    break;
+                case 1:
+                default:
+                    typeString = "item";
+                    tri = itemGrowth(context);
+            }
+
+            response.setContentType("text/csv; encoding='UTF-8'");
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.setHeader("Content-Disposition", "attachment; filename=growth-"+typeString+".csv");
+            writer = new CSVWriter(response.getWriter());
+
+            String[] firstRow = new String[3];
+            firstRow[0] = "YYYY-MM";
+            firstRow[1] = "count";
+            firstRow[2] = "total"+typeString;
+            writer.writeNext(firstRow);
 
             Integer total = 0;
             while(tri.hasNext()) {
                 TableRow row = tri.next();
-                //log.debug(row.toString());
                 String date = row.getStringColumn("yearmo");
                 Long count = row.getLongColumn("count");
                 total += count.intValue();
@@ -87,16 +105,18 @@ public class GrowthStatistics extends AbstractDSpaceTransformer
 
                 writer.writeNext(rowString);
             }
+            writer.close();
         } catch (SQLException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-
-        writer.close();
-
-
     }
 
-    protected TableRowIterator itemGrowth() {
+    @Override
+    public void generate() throws IOException, SAXException, ProcessingException {
+        // Had to move all work here to setup.
+    }
+
+    protected TableRowIterator itemGrowth(Context context) {
         String query = "SELECT to_char(date_trunc('month', t1.ts), 'YYYY-MM') AS yearmo, count(*) as count " +
                 "FROM ( SELECT to_timestamp(text_value, 'YYYY-MM-DD') AS ts FROM metadatavalue, item " +
                 "WHERE metadata_field_id = 12 AND metadatavalue.item_id = item.item_id AND item.in_archive=true	) t1 " +
@@ -105,14 +125,14 @@ public class GrowthStatistics extends AbstractDSpaceTransformer
         TableRowIterator tri = null;
 
         try {
-            tri = DatabaseManager.query(new Context(), query);
+            tri = DatabaseManager.query(context, query);
         } catch (SQLException e) {
             log.error(e.getMessage());
         }
         return tri;
     }
 
-    protected TableRowIterator bitstreamGrowth() {
+    protected TableRowIterator bitstreamGrowth(Context context) {
         String query = "select to_char(date_trunc('month', t1.ts), 'YYYY-MM') as yearmo, count(*) as count from\n" +
                 "(SELECT to_timestamp(text_value, 'YYYY-MM-DD') as ts \n" +
                 "FROM public.metadatavalue, public.item, public.item2bundle, public.bundle, public.bitstream, public.bundle2bitstream\n" +
@@ -124,10 +144,15 @@ public class GrowthStatistics extends AbstractDSpaceTransformer
         TableRowIterator tri = null;
 
         try {
-            tri = DatabaseManager.query(new Context(), query);
+            tri = DatabaseManager.query(context, query);
         } catch (SQLException e) {
             log.error(e.getMessage());
         }
         return tri;
+    }
+
+    public void recycle() {
+        this.response = null;
+        this.request = null;
     }
 }
